21335499538282ef3be81b0b649df4df869119ac
luan004 <38366327+luan004@users.noreply.github.com>
2022-07-05 23:19:17 -0300

    agr tem while

--- Exercícios de POO/Ex4/Main.java
+++ Exercícios de POO/Ex4/Main.java
@@ -9,28 +9,34 @@ public class Main {
 		
 		ControleRemoto cr = new ControleRemoto();
 		
-		System.out.print("O que você deseja fazer?\n\n1 - Aumentar o volume\n2 - Diminuir o volume\n3 - Avançar de canal\n4 - Retroceder de canal\n5 - Imprimir o valor do volume e do canal\n\nDigite: ");
-		int i = input.nextInt();
+		boolean t = true;
 		
-		if (i == 1) {
-			cr.maisVolume();
-			System.out.println(cr.imprimir());
+		while (t = true) {
+			System.out.print("\n\nO que você deseja fazer?\n\n1 - Aumentar o volume\n2 - Diminuir o volume\n3 - Avançar de canal\n4 - Retroceder de canal\n5 - Imprimir o valor do volume e do canal\n\nDigite: ");
+			int i = input.nextInt();
+			
+			if (i == 1) {
+				cr.maisVolume();
+				System.out.println(cr.imprimir());
+			}
+			else if (i == 2) {
+				cr.menosVolume();
+				System.out.println(cr.imprimir());
+			}
+			else if (i == 3) {
+				cr.avancarCanal();
+				System.out.println(cr.imprimir());
+			}
+			else if (i == 4) {
+				cr.retrocederCanal();
+				System.out.println(cr.imprimir());
+			}
+			else {
+				System.out.println(cr.imprimir());
+			}
 		}
-		else if (i == 2) {
-			cr.menosVolume();
-			System.out.println(cr.imprimir());
-		}
-		else if (i == 3) {
-			cr.avancarCanal();
-			System.out.println(cr.imprimir());
-		}
-		else if (i == 4) {
-			cr.retrocederCanal();
-			System.out.println(cr.imprimir());
-		}
-		else {
-			System.out.println(cr.imprimir());
-		}
+		
+
 
 	}
 
